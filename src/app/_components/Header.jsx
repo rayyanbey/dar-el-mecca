9e67008d21fb3f245be0406b7fa9f85b09dc5598
MuhammadKhizar89 <muhammadkhizar8919@gmail.com>
2025-01-03 13:05:08 +0500

    Navbar Completed

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -4,8 +4,50 @@ import Image from "next/image";
 import BrownButton, {Button} from "./BrownButton";
 import DropDown from "../_icons/DropDown";
 import {useState} from "react";
-
+import { usePathname } from "next/navigation";
 export function Header() {
+    const pathname = usePathname();
+    const [umrahPackages, setUmrahPackages] = useState([
+        {
+            name: "January Umrah Package",
+            link: "/umrah-packages/january-umrah",
+        },
+        {
+            name: "February Umrah Package",
+            link: "/umrah-packages/february-umrah",
+        },
+        {
+            name: "Ramadan 2nd Ashra Umrah",
+            link: "/umrah-packages/ramadan-2nd-ashra-umrah",
+        },
+        {
+            name: "All Umrah Packages",
+            link: "/umrah-packages/all-umrah-packages",
+        },
+    ]);
+    const [tourPackages, settourPackages] = useState([
+        {
+            name: "Turkey Tour Package",
+            link: "/tour-packages/turkey/",
+        },
+        {
+            name: "Egypt Tour Land Package",
+            link: "/tour-packages/egypt/",
+        },
+        {
+            name: "Dubai Tour Land Package",
+            link: "/tour-packages/dubai/",
+        },
+        {
+            name: "Jordan Amman Land Package",
+            link: "/tour-packages/jordan-amman/",
+        },
+        {
+            name: "All World Tour Packages",
+            link: "/tour-packages/all/",
+        },
+    ]);
+
     const [umrahDropDown, setUmrahDropDown] = useState(false);
     const [tourDropDown, setTourDropDown] = useState(false);
 
@@ -16,64 +58,91 @@ export function Header() {
                     <Image src="/logo.svg" alt="Dar El Mecca" width={60} height={73.3} quality={100} />
                 </Link>
                 <div className="flex gap-8 items-center font-[300] text-[16px]">
-                    <Link href="/" className="hover:text-secondary hover:underline hover:underline-offset-[5px]">
+                    <Link href="/" className={`hover:text-secondary ${pathname === "/" ? "text-secondary underline underline-offset-[5px] " : ""} hover:underline hover:underline-offset-[5px]`}>
                         Home
                     </Link>
-                    <Link
-                        onMouseEnter={() => setUmrahDropDown(true)}
-                        onMouseLeave={() => setUmrahDropDown(false)}
-                        href="/umrah-packages"
-                        className="flex gap-2 items-center relative"
-                    >
-                        <p className="hover:text-secondary hover:underline hover:underline-offset-[5px]">
+                    <p onMouseEnter={() => setUmrahDropDown(true)} className="flex gap-2 items-center relative">
+                        <span className={`hover:text-secondary ${pathname.includes("umrah-packages") || pathname === "/umrah-packages" ? "text-secondary underline underline-offset-[5px] " : ""}  hover:underline hover:underline-offset-[5px]`}>
                             Umrah Packages
-                        </p>
+                        </span>
                         <DropDown />
                         {umrahDropDown && (
-                            <div className="absolute top-full -left-14 mt-2 w-56 bg-black text-white rounded-lg shadow-lg z-50">
-                                <ul className="flex flex-col items-center gap-2 py-4">
-                                    <li className="hover:bg-primary" >
-                                        <Link href="/umrah-packages/january" className="font-[300] text-[16px]">
-                                            January Umrah Package
-                                        </Link>
-                                    </li>
-                                    <div className="w-full h-[1px] bg-[#FFFFFF33]"></div>
-                                    <li>
-                                        <Link href="/umrah-packages/february" className="  font-[300] text-[16px]">
-                                            February Umrah Package
-                                        </Link>
-                                    </li>
-                                    <div className="w-full h-[1px] bg-[#FFFFFF33]"></div>
-                                    <li>
-                                        <Link href="/umrah-packages/ramadan" className="font-[300] text-[16px]">
-                                            Ramadan 2nd Ashra Umrah
-                                        </Link>
-                                    </li>
-                                    <div className="w-full h-[1px] bg-[#FFFFFF33]"></div>
-                                    <li>
-                                        <Link href="/umrah-packages/all" className="font-[300] text-[16px]">
-                                            All Umrah Packages
-                                        </Link>
-                                    </li>
+                            <div
+                                onMouseLeave={() => setUmrahDropDown(false)}
+                                className="absolute top-full -left-14 mt-2 w-56 bg-black text-white rounded-lg shadow-lg z-50"
+                            >
+                                <ul className="flex flex-col ">
+                                    {umrahPackages.map((item, index) => (
+                                        <div>
+                                            <li
+                                                key={index}
+                                                className={`w-full flex flex-col justify-center items-center hover:bg-primary ${
+                                                    index == 0
+                                                        ? "hover:rounded-t-lg"
+                                                        : index == umrahPackages.length - 1
+                                                        ? "hover:rounded-b-lg"
+                                                        : ""
+                                                }  py-2`}
+                                            >
+                                                <Link href={item.link} className="p-1 font-[300] text-[16px] text-center">
+                                                    {item.name}
+                                                </Link>
+                                            </li>
+                                            {index < umrahPackages.length - 1 && (
+                                                <div className="w-full h-[1px] bg-[#FFFFFF33]"></div>
+                                            )}
+                                        </div>
+                                    ))}
                                 </ul>
                             </div>
                         )}
-                    </Link>
+                    </p>
+
                     <Link
                         href="/hajj-packages"
                         className="hover:text-secondary hover:underline hover:underline-offset-[5px] "
                     >
                         Hajj Packages (Coming Soon)
                     </Link>
-                    <Link href="/tours" className="flex gap-2 items-center">
-                        <p className="hover:text-secondary hover:underline hover:underline-offset-[5px]">
-                            Tours & Packages
-                        </p>
+                    <p onMouseEnter={() => setTourDropDown(true)} className="flex gap-2 items-center relative">
+                        <span className="hover:text-secondary hover:underline hover:underline-offset-[5px]">
+                            Tour & Packages
+                        </span>
                         <DropDown />
-                    </Link>
+                        {tourDropDown && (
+                            <div
+                                onMouseLeave={() => setTourDropDown(false)}
+                                className="absolute top-full -left-10 mt-2 w-56 bg-black text-white rounded-lg shadow-lg z-50"
+                            >
+                                <ul className="flex flex-col ">
+                                    {tourPackages.map((item, index) => (
+                                        <div>
+                                            <li
+                                                key={index}
+                                                className={`w-full flex flex-col justify-center items-center hover:bg-primary ${
+                                                    index == 0
+                                                        ? "hover:rounded-t-lg"
+                                                        : index == tourPackages.length - 1
+                                                        ? "hover:rounded-b-lg"
+                                                        : ""
+                                                }  py-2`}
+                                            >
+                                                <Link href={item.link} className="p-1 font-[300] text-[16px] text-center">
+                                                    {item.name}
+                                                </Link>
+                                            </li>
+                                            {index < tourPackages.length - 1 && (
+                                                <div className="w-full h-[1px] bg-[#FFFFFF33]"></div>
+                                            )}
+                                        </div>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                    </p>
                     <Link
-                        href="/resources"
-                        className="hover:text-secondary hover:underline hover:underline-offset-[5px]"
+                        href="/hajj-and-umrah-guides"
+                        className={`hover:text-secondary ${pathname === "/hajj-and-umrah-guides" ? "text-secondary underline underline-offset-[5px] " : ""} hover:underline hover:underline-offset-[5px]`}
                     >
                         Hajj and Umrah Guides
                     </Link>
