20d85f29b38567d7e99abd3bf38bfb1ea57f49d8
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-01-11 18:37:19 +0500

    Fix deleteEventById route to use 'snug' parameter instead of 'id'

--- src/app/pages/apis/events/deleteEventById/[snug]/route.js
+++ src/app/pages/apis/events/deleteEventById/[snug]/route.js
@@ -1,4 +1,5 @@
-import { deleteEvent } from "../../../controllers/event.controller";
+import { deleteEvent } from "../../../../controllers/event.controller"
+
 export const POST = async (req, {params}) => {
-    return await deleteEvent(req,params.id);
+    return await deleteEvent(req,params.snug)
 }
