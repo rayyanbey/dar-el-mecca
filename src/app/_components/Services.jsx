4678d1d7f9a86826bb70effea32549d8d536d9e5
Muhammad Khizar <muhammadkhizar8919@gmail.com>
2025-01-19 17:19:48 +0500

    All Done

--- src/app/_components/Services.jsx
+++ src/app/_components/Services.jsx
@@ -2,18 +2,21 @@ import ServiceCard from "./ServiceCard";
 
 const services = [
     {
+        type: "U",
         title: "Umrah Packages",
         description:
             "Discover Umrah services tailored for USA and all over the world. We handle visas, accommodations, and transport, ensuring a hassle-free spiritual journey.",
         image: "/images/umrah-service.jpeg",
     },
     {
+        type: "H",
         title: "Hajj Packages",
         description:
             "Embark on a tailored Hajj journey with Darelmecca Travel where affordability meets top notch service, ensuring comfort and spiritual fulfilment.",
         image: "/images/hajj-service.jpeg",
     },
     {
+        type: "T",
         title: "Tour Packages",
         description:
             "Embark on a Journey of Faith with Darelmecca Travel. Our tours ensure seamless, affordable pilgrimages for a transformative spiritual experience.",
@@ -29,9 +32,15 @@ export function Services() {
                 We provide 3 types of tours for our clients, We arrange high quality tours with exceptional support.
             </p>
             <div className="flex flex-col lg:flex-row justify-center flex-wrap gap-10 py-4 ">
-            {services.map((service) => (
-               <ServiceCard key={service.title} title={service.title} description={service.description} image={service.image}/>
-            ))}
+                {services.map((service) => (
+                    <ServiceCard
+                        key={service.title}
+                        title={service.title}
+                        description={service.description}
+                        image={service.image}
+                        type={service.type}
+                    />
+                ))}
             </div>
         </section>
     );
