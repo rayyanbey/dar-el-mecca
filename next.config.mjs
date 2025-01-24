f6de1831c7e0658108ecbe332c32a530f89d5f4a
Muhammad Khizar <muhammadkhizar8919@gmail.com>
2025-01-24 23:30:25 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
-// / next.config.js
+// next.config.js
 
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
+                // Apply these headers to all API routes
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" }, // replace this with your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
@@ -18,4 +18,4 @@ const nextConfig = {
     }
 }
 
-export default nextConfig
+export default nextConfig;
