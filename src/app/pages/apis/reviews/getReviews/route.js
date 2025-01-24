4f8a6fea589144bf6373888d03df85bcc9c61ea6
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-01-24 19:30:58 +0500

    Add CORS support: implement middleware for handling CORS headers and preflight requests for API routes

--- src/app/pages/apis/reviews/getReviews/route.js
+++ src/app/pages/apis/reviews/getReviews/route.js
@@ -1,5 +1,5 @@
 import { getReviews } from "../../../controllers/review.controller";
 
 export const GET = async (req, res) => {
-    return await getReviews(req, res)
-}
+  return await getReviews(req, res);
+};
