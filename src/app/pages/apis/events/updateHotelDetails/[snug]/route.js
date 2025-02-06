f5747822413c90ae4bba79fcdf0560833853508b
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-02-06 16:28:46 +0500

    testing

--- src/app/pages/apis/events/updateHotelDetails/[snug]/route.js
+++ src/app/pages/apis/events/updateHotelDetails/[snug]/route.js
@@ -2,23 +2,5 @@ import { NextResponse } from "next/server";
 import { updateHotelDetails } from "../../../../controllers/event.controller";
 
 export const PUT = async (req, { params }) => {
-  const { snug } = params;
-  try {
     const success = await updateHotelDetails(req, snug);
-    if (success) {
-      return NextResponse.json(
-        { message: "Hotel details updated successfully" },
-        { status: 200 }
-      );
-    }
-    return NextResponse.json(
-      { message: "Failed to update hotel details" },
-      { status: 400 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Internal Server Error", error: error.message },
-      { status: 500 }
-    );
-  }
 };
