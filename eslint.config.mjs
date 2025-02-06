4b96e36e37619b389c581e33e71824a9dd977ba1
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-02-07 00:38:55 +0500

    fix: refactor eslint configuration to use __dirname and simplify extends

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  // Disable caching explicitly
-  { cache: false },
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
