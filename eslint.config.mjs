532e379c6142fb1896492155cdaf78999502625b
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-02-08 00:52:38 +0500

    refactor: simplify ESLint configuration by removing custom parser and parser options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,4 @@ const compat = new FlatCompat({
 
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
-export default {
-  parser: "custom-eslint-parser",
-  parserOptions: { ecmaVersion: 2020 },
-  // …
-};
+export default eslintConfig;
