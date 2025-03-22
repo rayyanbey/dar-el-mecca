b98ba753faee1e5b2d9a2121ef6dae137e7617c7
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2025-03-22 14:25:17 +0500

    fix: update CORS policy to allow requests from the production admin dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
           {
             source: "/pages/apis/:path*",
             headers: [
-              { key: "Access-Control-Allow-Origin", value: "http://localhost:3001" },
+              { key: "Access-Control-Allow-Origin", value: "https://admin-dashboard-green-sigma.vercel.app/" },
               { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
               { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
             ],
